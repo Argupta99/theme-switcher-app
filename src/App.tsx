a6c640413f45985acb00624de36a10e53707c38d
Argupta99 <178310155+Argupta99@users.noreply.github.com>
2025-08-01 00:16:13 +0530

    Initial commit - Theme Switcher App

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Header from './Components/Header';
+
+
+function App() {
+ return (
+  <>
+
+  <Header />
+<main className='min-h-screen pt-5  transition-all duration-300 ease-in-out'>
+  
+  <Routes>
+    <Route path='/' element = {<Home />} />
+    <Route path = '/about' element = {<About />} />
+    <Route path = '/contact' element = {<Contact />} />
+    </Routes>
+
+  </main>
+  
+  
+  </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+function Home () {
+    return (
+        <h1 className="text-3xl font-bold text-red-500 bg-yellow-100 p-4 rounded-lg">
+  Home
+</h1>
+    );
+}
+
+export default Home;
