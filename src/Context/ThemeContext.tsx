a4b8099dc0a49c4df4127a03c727dedc47cd4923
Argupta99 <178310155+Argupta99@users.noreply.github.com>
2025-08-02 22:15:41 +0530

    useTheme

--- src/Context/ThemeContext.tsx
+++ src/Context/ThemeContext.tsx
@@ -1,5 +1,33 @@
-function ThemeProvider ({  children }) {
-  return children;
+
+import {  useState } from "react";
+import { createContext, useContext } from "react";
+import type { ReactNode } from "react";
+
+
+const ThemeContext = createContext({
+  theme:"light",
+  ChangeTheme: (theme: string) => {},
+})
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+
+  //state to hold the current theme 
+
+  const [theme, setTheme] = useState("light");
+
+  const changeTheme = (newTheme: string) => {
+    setTheme(newTheme);
+    document.documentElement.setAttribute("data-theme", newTheme);
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme, ChangeTheme: changeTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+
+ 
+
 }
 
-export { ThemeProvider };
+export const useTheme = () => useContext(ThemeContext);
