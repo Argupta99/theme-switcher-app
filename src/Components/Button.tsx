fb00a078e8ba668b7476f4875d7c6b2e53ae868e
Argupta99 <178310155+Argupta99@users.noreply.github.com>
2025-08-04 01:33:28 +0530

    button

---
+++ src/Components/Button.tsx
@@ -0,0 +1,15 @@
+type ButtonProps = {
+    text: string;
+    onClick?: () => void;
+
+}
+
+function Button ({text, onClick}: ButtonProps) {
+    return (
+        <button onClick={onClick} className="px-4 py-2 bg-[var(--accent-color)] text-white rounded hover:scale-105 transition">
+            {text}
+        </button>
+    )
+}
+
+export default Button;
