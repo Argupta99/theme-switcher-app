76fb6720f0e6d5f764b662076e21c200d4b51b56
Argupta99 <178310155+Argupta99@users.noreply.github.com>
2025-08-03 11:40:36 +0530

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
 import Header from './Components/Header';
+import Details from './pages/Details';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
     <Route path='/' element = {<Home />} />
     <Route path = '/about' element = {<About />} />
     <Route path = '/contact' element = {<Contact />} />
+    <Route path = "/details/:id" element = {<Details />} />
     </Routes>
 
   </main>
