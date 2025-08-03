3e6f37c42d09a710aaf176efe9ba5d9b6716d749
Argupta99 <178310155+Argupta99@users.noreply.github.com>
2025-08-03 11:35:40 +0530

    cards formed

--- src/Components/Cards.tsx
+++ src/Components/Cards.tsx
@@ -1,18 +1,25 @@
-type CardProps = {title: string, description: string, image: string; 
+import { Link } from "react-router-dom";
+
+type CardProps 
+= {id: string, 
+    title: string,
+ description: string; 
 };
 
 
-function card ({title, description}: CardProps) {
+function Card ({id,title, description}: CardProps) {
 
     return (
+        <Link to={`/details/${id}`}>
         <div>
             <h3>{title}</h3>
             <p>{description}</p>
         </div>
+        </Link>
     );
 
 }
 
-export default card;
+export default Card;
 
 
