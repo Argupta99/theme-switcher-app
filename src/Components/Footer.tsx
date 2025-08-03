00e260fd1fc237d627f22568f9ebdc328a126f1d
Argupta99 <178310155+Argupta99@users.noreply.github.com>
2025-08-04 00:56:20 +0530

    Footer

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer () {
+    return (
+        <footer className="bg-[var(--footer-bg)] text-[var(--text-color) text-center py-4 mt-10 text-sm]">
+          <p>© 2025 Theme Switcher</p>  
+        </footer>
+
+    )
+}
+
+export default Footer;
