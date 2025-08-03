6b44d2e8d5318e927803b2da6d53ef8947139232
Argupta99 <178310155+Argupta99@users.noreply.github.com>
2025-08-03 11:56:01 +0530

    dummy data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,42 @@
-import card from "../Components/Cards";
+import Card from "../Components/Cards";
 
 //makeing an array of data to pass to the card components
 
 const cardData = [
     {
-        title: "Card 1",
-        description: "This is the first card.",
+        id: "1",
+        title: "CHimalayan Stillness at Kunzum Pass",
+        description: "Where the sky is a shade of blue you've never seen before. Colorful prayer flags flutter in the cold, thin air, sending whispers across the stark, majestic peaks of Spiti Valley. A place where time slows, and the world feels pure.",
        
     },
     {
-        title: "Card 2",
-        description: "This is the second card.",
+        id: "2",
+        title: "The Unfinished Canvas",
+        description: "The beautiful chaos of a new idea. The first bold stroke of cobalt blue on a blank canvas, surrounded by terracotta pots, scattered notes, and the promise of a story waiting to be told.",
         
     },
     {
-        title: "Card 3",
-        description: "This is the third card.",
+        id:"3",
+        title: "Sunday Chai & Cardamom",
+        description: "There's a special kind of magic in the first sip of chai on a lazy Sunday. The warmth of the cup, the fragrant steam carrying notes of ginger and cardamom, and the soft rhythm of rain outside.",
         
     }
 ]
+
+function Home () {
+    return (
+
+        <main>
+            <h1>HomePage</h1>
+
+           {cardData.map((card, index) => (
+        <Card 
+        key = {index}
+        title= {card.title}
+        description= {card.description} />
+       ) )}
+        </main>
+    );
+}
 
 export default Home;
