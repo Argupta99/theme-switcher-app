3f0603a2af89825bc4f98870c043de9bb92cb2e0
Argupta99 <178310155+Argupta99@users.noreply.github.com>
2025-08-04 06:32:22 +0530

    persisting theme

--- src/Context/ThemeContext.tsx
+++ src/Context/ThemeContext.tsx
@@ -1,18 +1,23 @@
 import { useState, createContext, useContext, useEffect } from "react";
 import type { ReactNode } from "react";
 
+// Create a ThemeContext with default values
+// Includes the current theme and a placeholder for the theme-changing function
+
 const ThemeContext = createContext({
   theme: "light",
-  ChangeTheme: (theme: string) => {}, 
+  ChangeTheme: (_theme: string) => {}, 
 });
 
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
   
+    
+  // Initialize theme from localStorage (if available) or default to "light"
   const [theme, setTheme] = useState(() => {
     return localStorage.getItem("app-theme") || "light";
   });
 
-  
+  // Update <html> attribute and persist theme to localStorage whenever theme changes
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
     localStorage.setItem("app-theme", theme);
